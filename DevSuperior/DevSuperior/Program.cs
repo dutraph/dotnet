9f261df2546d3ed66180c85082ff6241c64e0b80
Paulo Silva <dutra.phs@gmail.com>
2022-08-26 18:41:58 +0100

    Starting project

---
+++ DevSuperior/DevSuperior/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Program {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello, World!");
+
+            bool complete = false;
+            char genre = 'F';
+            sbyte s = 10;
+            byte b = 126; //Não aceita numero negativo
+            int i = 2147483647;
+            long l = 2147483648L;
+            float f = 4.5f;
+            double d = 4.5;
+            string nome = "Paulo"; //Imutavel
+            object nome2 = "Paulo Dutra";
+            nome2 = "Paulo Silva";
+            nome = "Joao";
+
+
+            Console.WriteLine(s);
+            Console.WriteLine(b);
+            Console.WriteLine(i);
+            Console.WriteLine(l);
+            Console.WriteLine(complete);
+            Console.WriteLine(genre);
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(nome);
+            Console.WriteLine(nome2);
+            Console.WriteLine(int.MaxValue);
+        }
+    }
+}
